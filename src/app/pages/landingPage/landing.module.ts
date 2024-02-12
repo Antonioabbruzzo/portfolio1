a38913410ff053d1a78b2c31facc7be048e767ed
Antonioabbruzzo <abbruzzo.antonio95@gmail.com>
2024-02-12 16:33:50 +0100

    styling interface

--- src/app/pages/landingPage/landing.module.ts
+++ src/app/pages/landingPage/landing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LandingComponent } from 'src/app/pages/landingPage/landing/landing.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LandingComponent],
   imports: [
     CommonModule,
     LandingRoutingModule, SharedModule,
--- src/app/pages/landingPage/landing/landing.component.ts
+++ src/app/pages/landingPage/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -7,5 +7,5 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class LandingComponent {
-  products: any[] = [1, 2, 3, 4, 5, 6, 7, 7];
+
 }
